f1743daaaf01d1faba1c7e3878ce3e7a32875240
Anton <office.sklyarov@gmail.com>
2020-09-24 15:22:01 +0300

    corrected buttons and added anchors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,5 @@
 'use strict';
 
-const contactUsButton = document.querySelector('.header__button');
-
-contactUsButton.addEventListener('click', event => {
-  window.open('tel:+00 123 456 789');
-  event.preventDefault();
-});
-
 document.querySelector('.mobile-menu').onclick = () => {
   document.querySelector('.navigation__toggler').checked = false;
 };
