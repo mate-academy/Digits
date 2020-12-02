0d836c4ed0255d1716f617e13e80a36572fca646
Dmytro Tsehelnyk <34zimnii@gmail.com>
2020-12-02 09:49:13 +0200

    created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burgerMenu = document.querySelector('#burgerMenu');
+const burgerMenuClasses = burgerMenu.classList;
+const headerMenuClasses = document.querySelector('#headerMenu').classList;
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenuClasses.toggle('header__burger--active');
+  headerMenuClasses.toggle('header__right--active');
+});
