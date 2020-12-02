a97709592342a0d15ebe53fcf9ca10242b3410e1
Dmytro Tsehelnyk <34zimnii@gmail.com>
2020-12-02 16:11:27 +0200

    responsive + animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,25 @@ burgerMenu.addEventListener('click', () => {
   burgerMenuClasses.toggle('header__burger--active');
   headerMenuClasses.toggle('header__right--active');
 });
+
+// smooth scroll
+$(document).ready(function() {
+  // Add smooth scrolling to all links
+  $('a').on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top,
+      }, 500, function() {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
