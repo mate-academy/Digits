b3bf762d14a50c960700ccb782f0bb3a2a7fdd6f
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-06-15 17:24:43 +0300

    Added header with adaptability and burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const hamburger = document.querySelector('.hamburger');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const burgerLink = document.querySelector('.burger-nav__list');
+
+hamburger.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('hamburger-menu--active');
+  hamburger.classList.toggle('hamburger--active');
+});
+
+burgerLink.addEventListener('click', () => {
+  hamburgerMenu.classList.remove('hamburger-menu--active');
+  hamburger.classList.remove('hamburger--active');
+});
